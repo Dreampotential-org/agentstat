3eaa7aedeb4fb603e1174e4927a5b6312b6273e6
Aaron Rosen <aaronorosen@gmail.com>
2019-12-10 16:02:48 -0800

    more

--- www/js/main.js
+++ www/js/main.js
@@ -1,37 +1,58 @@
-function init() {
-  var input = document.getElementById('search_input');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.addListener('place_changed', fillIn);
-
-  function getZipcode(place) {
-    console.log(place)
-    $("#formatted_address").val(place.formatted_address)
-    for(var address_comp of place.address_components) {
-         console.log(address_comp.types)
-      if (address_comp.types[0] == "administrative_area_level_1") {
-            console.log("City: " + address_comp.short_name)
-      }
-      if (address_comp.types[0] == 'locality') {
-            console.log("City: " + address_comp.long_name)
-        /*if (!(cities.includes(address_comp.long_name))) {
-          swal("No data for city found for " + address_comp.long_name,
-               "At this time we are only live in washington State",
-               "error")
-          $("#search_input").val("");
-          return
-        } */
-        $("#city").val(address_comp.long_name);
-        break
-      }
-    }
-    $('form#filterForm').submit();
-  }
-  function fillIn() {
+function fillIn() {
     var place = this.getPlace();
     var addr = place.formatted_address
 
-    getZipcode(place)
-  }
+    var results = getSearchParams(place)
+    console.log(results)
+    redirectResults(results)
+}
+
+function redirectResults(results) {
+    if ('city' in results) {
+        window.location = (
+            'https://agentstat.com/reports_v2/' + results['state'] +
+            '/?city=' + results['city']
+        )
+    } else {
+        // XXX hard coded in WA for agents state
+        window.location = (
+            'https://agentstat.com/reports_v2/WA' +
+            '/?agent_name_or_id=' + results['agent_name']
+        )
+    }
+
+}
+
+function getSearchParams(place) {
+
+    console.log(place)
+    var params = {}
+    if (!('scope' in place) && 'name' in place) {
+        params['agent_name'] = place.name
+        return params
+    }
+    for(var address_comp of place.address_components) {
+        console.log(address_comp.types)
+        if (address_comp.types[0] == "administrative_area_level_1") {
+            console.log("City: " + address_comp.short_name)
+            params['state'] = address_comp.short_name
+        }
+        if (address_comp.types[0] == 'locality') {
+            params['city'] = address_comp.short_name
+        }
+    }
+    console.log(params)
+    return params
+}
+
+function init() {
+    var input = document.getElementById('search_input');
+    var options = {
+        types: ['address'],
+    }
+
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete.addListener('place_changed', fillIn);
 
   $("body").delegate("#go", "click", function(e) {
     $("#agent_name_or_id").val($("#search_input_agent").val())
@@ -52,10 +73,7 @@ function init() {
     if (e.which == 13 && $('.pac-container:visible').length) return false;
 
     if (e.keyCode == 13) {
-      e.preventDefault()
-      $('form#filterForm').submit();
     }
   })
-
 }
 window.addEventListener("DOMContentLoaded", init, false);
