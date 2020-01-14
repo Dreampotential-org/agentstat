c95413192c3c0dbefe6580c11840d1091a9ec1cc
Aaron Rosen <aaronorosen@gmail.com>
2020-01-13 17:45:13 -0800

    more fixes

--- www/js/search.js
+++ www/js/search.js
@@ -1,55 +1,76 @@
-settings = get_settings('reports/WA/?city=Seattle&page=1', 'GET');
-settings['headers'] = null;
-// Example requests
-// reports/WA/Seattle/?duration=12&home_type=SINGLE_FAMILY
-var data;
-var search_result = '';
+function init() {
+    load_search_results()
+}
 
+function get_search_filters() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const city = urlParams.get('city')
+    var filters = ''
+    if (city != null) {
+        filters = 'city=' + city
+    }
+    return filters
+}
 
-$.ajax(settings).done(function (response) {
-  data = JSON.parse(response);
-  results = data['results'];
-  $.each(results, function(k, v) {
-    item = search_item.split('[[agent_name]]').join(v['agent_full_name']);
-    item = item.split('[[time_duration]]').join(v['time_duration']);
-    // item = item.split('[[cell_phone]]').join(v['agent_cell_phone']);
-    // item = item.split('[[cel_phone]]').join(v['agent_cell_phone']);
-    item = item.split('[[city]]').join(v['city']);
-    item = item.split('[[score]]').join(v['score']);
-    item = item.split('[[agent_id]]').join(v['agent_id']);
-    item = item.split('[[agent_full_name]]').join(v['agent_full_name']);
+function load_search_results() {
+    var filters = get_search_filters()
+    var url = ('reports/WA/?' + filters + '&page=1')
+    console.log(url)
+    settings = get_settings('reports/WA/?' + filters + '&page=1', 'GET');
+    settings['headers'] = null;
+    // Example requests
+    // reports/WA/Seattle/?duration=12&home_type=SINGLE_FAMILY
+    var data;
+    var search_result = '';
 
-    item = item.split('[[overall_failed_listings]]').join(v['overall_failed_listings']);
-    item = item.split('[[failed_listings]]').join(v['failed_listings']);
+    $.ajax(settings).done(function (response) {
 
-    item = item.split('[[overall_sold_listings]]').join(v['overall_sold_listings']);
-    item = item.split('[[sold_listings]]').join(v['sold_listings']);
+      data = JSON.parse(response);
+      results = data['results'];
 
-    item = item.split('[[overall_avg_dom]]').join(
-        v['overall_avg_dom'].toFixed(1));
-    item = item.split('[[avg_dom]]').join(v['avg_dom'].toFixed(1));
+      $.each(results, function(k, v) {
+        item = search_item.split('[[agent_name]]').join(v['agent_full_name']);
+        item = item.split('[[time_duration]]').join(v['time_duration']);
+        item = item.split('[[city]]').join(v['city']);
+        item = item.split('[[score]]').join(v['score']);
+        item = item.split('[[agent_id]]').join(v['agent_id']);
+        item = item.split('[[agent_full_name]]').join(v['agent_full_name']);
 
-    item = item.split('[[overall_s2l_price]]').join(
-        v['overall_s2l_price'].toFixed(1));
-    item = item.split('[[s2l_price]]').join(v['s2l_price'].toFixed(1));
+        item = item.split('[[overall_failed_listings]]').join(
+            v['overall_failed_listings']);
+        item = item.split('[[failed_listings]]').join(v['failed_listings']);
 
-    item = item.split('[[overall_listings_breakdown_json]]').join(
-        JSON.parse(v['overall_listings_breakdown_json']));
-    item = item.split('[[listings_breakdown_json]]').join(
-        JSON.parse(v['listings_breakdown_json']));
+        item = item.split('[[overall_sold_listings]]').join(
+            v['overall_sold_listings']);
+        item = item.split('[[sold_listings]]').join(v['sold_listings']);
 
-    search_result += item;
-  });
+        item = item.split('[[overall_avg_dom]]').join(
+            v['overall_avg_dom'].toFixed(1));
+        item = item.split('[[avg_dom]]').join(v['avg_dom'].toFixed(1));
 
-  $('#result-count').html(data['total']);
-  $('#page-section').html(search_result);
+        item = item.split('[[overall_s2l_price]]').join(
+            v['overall_s2l_price'].toFixed(1));
+        item = item.split('[[s2l_price]]').join(v['s2l_price'].toFixed(1));
 
-}).fail(function(err) {
-  // alert('Got err');
-  $('.msg').html(err['responseText']);
-  $('.msg').css("display", "block");
-  console.log(err);
-});
+        item = item.split('[[overall_listings_breakdown_json]]').join(
+            JSON.parse(v['overall_listings_breakdown_json']));
+        item = item.split('[[listings_breakdown_json]]').join(
+            JSON.parse(v['listings_breakdown_json']));
+
+        search_result += item;
+      });
+
+      $('#result-count').html(data['total']);
+      $('#page-section').html(search_result);
+
+    }).fail(function(err) {
+      // alert('Got err');
+      $('.msg').html(err['responseText']);
+      $('.msg').css("display", "block");
+      console.log(err);
+    });
+
+}
 
 $(document).on('change click', '.lead-submit', function() {
   var selected_agent_id = $(this).attr('data-id');
@@ -76,3 +97,6 @@ $(document).on('change click', '.lead-submit', function() {
   });
 
 });
+
+
+window.addEventListener("DOMContentLoaded", init, false);
