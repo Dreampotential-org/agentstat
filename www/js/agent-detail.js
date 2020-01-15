14dc12ad1c8bea8b64925d1287cd53ee3e67e647
adem oztas <adem@oztas.net>
2020-01-15 17:40:28 +0300

    implement leads form

---
+++ www/js/agent-detail.js
@@ -0,0 +1,98 @@
+
+function init() {
+    console.log('TESTSSSSSSS');
+    load_agent();
+}
+
+function load_agent() {
+  const urlParams = new URLSearchParams(window.location.search)
+  var agent_id =  urlParams.get('agent_id')
+
+  settings = get_settings('agents/' + agent_id, 'GET');
+  settings['headers'] = null;
+
+
+  $.ajax(settings).done(function (response) {
+    data = JSON.parse(response);
+    console.log(data);
+    $('.agent_name').val(data['agent_name']);
+    $.each($('.agent_name'), function() { $(this).html(data['full_name']) });
+    var name_city = data['full_name'] + ' - ' + data['city'];
+
+    $.each($('.agent_name_city'), function() { $(this).html( name_city )});
+
+    console.log(data['city']);
+
+  }).fail(function(err){
+    console.log(err);
+  });
+}
+
+$(document).on('change click', '.how_soon li>a', function() {
+
+  console.log($(this).text());
+
+  var leads = {
+    'how_soon_sell': $(this).text()
+  }
+  localStorage.setItem('leads', JSON.stringify(leads));
+
+  $('#leads-step-one').css('display', 'None');
+  $('#leads-step-two').css('display', 'block');
+
+});
+
+$(document).on('change click', '.why_interest li>a', function() {
+  var data = JSON.parse(localStorage.getItem('leads'));
+  data['interest_reason'] = $(this).text();
+
+  localStorage.setItem('leads', JSON.stringify(data));
+
+  $('#leads-step-two').css('display', 'none');
+  $('#leads-step-three').css('display', 'block');
+
+  localStorage.setItem('leads', JSON.stringify(data));
+
+  console.log('tests');
+
+});
+
+$(document).on('change click', '#lead-submit', function() {
+  const urlParams = new URLSearchParams(window.location.search)
+  var agent_id =  urlParams.get('agent_id')
+  var data = {};
+
+  data = JSON.parse(localStorage.getItem('leads'));
+
+  data['email'] = $('#lead_email').val();
+  data['name'] = $('#lead_name').val();
+  data['phone'] = $('#lead_phone').val();
+  data['agent'] = agent_id;
+
+  if (data['email'] === '' || data['name'] === '' || data['phone'] === '') {
+    console.log('All fields are required.');
+    $('.msg').html('All fields are required');
+    return false;
+  }
+
+  localStorage.setItem('leads', JSON.stringify(data));
+
+  console.log(data);
+
+  settings = get_settings('lead/', 'POST', JSON.stringify(data));
+  settings['headers'] = null;
+
+  $.ajax(settings).done(function (response) {
+    var msg = JSON.parse(response);
+    console.log('success');
+    $('#leads-step-three').css('display', 'none');
+    $('#leads-step-four').css('display', 'block');
+
+  }).fail(function(err) {
+    $('.msg').html(err['responseText']);
+    console.log(err);
+  });
+});
+
+
+window.addEventListener("DOMContentLoaded", init, false);
