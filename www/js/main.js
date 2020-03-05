e0e7b932f0a2c31c0cf14c42afbcf522aaf6e5b0
Aaron Rosen <aaronorosen@gmail.com>
2020-03-04 17:40:55 -0800

    first version of error reporting

--- www/js/main.js
+++ www/js/main.js
@@ -19,11 +19,10 @@ function set_search_input() {
     }
     var type = urlParams.get('type')
     var city = urlParams.get('city')
-    
     $("#city-search-filter").val(city)
     $(".ser").val(agent_name)
 
-    $('.seller-filter input[type="radio"]'). each(function(){ 
+    $('.seller-filter input[type="radio"]'). each(function() {
       //console.log($(this).is(":checked")+" "+$(this). is(":not(:checked)"));
       if($(this).val()== type){
         $(this). prop("checked", true);
@@ -299,6 +298,29 @@ $(document).ready(function () {
 
 });
 
+// on error tell us about it.
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    var url = 'https://hooks.slack.com/services/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'
+    if (string.indexOf(substring) > -1){
+        alert('Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join(' - ');
+
+        $.post(url, JSON.stringify({"text": message}), function() {
+            console.log("reported to slack")
+        });
+
+    }
+    return false;
+};
 
 //  Start of Async Drift Code
 !function() {
