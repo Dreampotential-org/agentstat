a4376015ed73d7747f93a639a40255092b4a7273
Vijay <websitedesignanddeveloper@gmail.com>
2020-03-17 20:15:45 +0530

    search bar and login

--- www/js/type.js
+++ www/js/type.js
@@ -11,29 +11,53 @@
 		    	$(".type-value").text (a);
 				$(".check-type").slideUp();
 			});
-			$(".index-address-type input[type=radio]").click(function (){
-		    	var a = $(this).val();
+			$(".index-address-type ul li").click(function (){
+			
+				var a = $(this).text();
 		    	$(".address-value").html (a+" <i class='fas fa-caret-down'></i>");
 				$(".address-type").slideUp();
 			});
+			$(".index-address-map-type ul li").click(function (){
+			
+				var a = $(this).attr('value');
+		    	$(".address-map-value").html (a+" <i class='fas fa-caret-down'></i>");
+				$(".address-map-type").slideUp();
+			});
 
-			$(".ser-state-type input[type=radio]").click(function (){
-		    	var a = $(this).val();
+			$("#statesData").on('click','li',function (){
+		    	var a = $(this).text();
+		    	$(".state-map-value").html (a+" <i class='fas fa-caret-down'></i>");
+				$(".ser-state-map-type").slideUp();
+			});
+
+			$("#allstate").on('click','li', function (){
+				var a = $(this).text();
 		    	$(".state-value").html (a+" <i class='fas fa-caret-down'></i>");
 				$(".ser-state-type").slideUp();
 			});
+			
 			
 
 		    $("input[type=radio]").click(function (){
 		    	var aa = $(this).val();
 		    	// $(".type-value").text (aa);
 				$(".check-type").slideUp();
+		    });
+			
+			$(".index-address-type ul li").click(function (){
+		    	var aa = $(this).val();
 				// $(".address-value").text (aa);
 				$(".address-type").slideUp();
 				
 				$(".ser-state-type").slideUp();
+			});
+			$(".index-address-map-type ul li").click(function (){
+		    	var aa = $(this).val();
+				// $(".address-value").text (aa);
+				$(".address-map-type").slideUp();
+				
+				$(".ser-state-map-type").slideUp();
 		    });
-			
 		    $(".bau1-cotent input[type=radio]").click(function (){
 		    	var b = $(this).val();
 		    	$(".bau-in").text (b);
