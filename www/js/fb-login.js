59129f2b315481ffe7a963bd057e3cda34ce83a8
adem oztas <adem@oztas.net>
2020-06-23 10:42:33 -0400

    fb login [WIP]

---
+++ www/js/fb-login.js
@@ -0,0 +1,36 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '265799321171235',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v7.0'
+  });
+    
+  FB.AppEvents.logPageView();   
+    
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+
+$(document).ready(function () {
+
+  FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+      console.log('XXX: ' + response);
+  });
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+      console.log(response);
+    });
+  }
+
+});
