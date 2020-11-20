8ec680b6989469b40b64e9f8e3abd1dfe78ba4c9
Hassan Ejaz <hassanejaz22@gmail.com>
2020-11-20 06:19:21 +0500

    marketing partner

--- www/js/marketing.js
+++ www/js/marketing.js
@@ -9,6 +9,31 @@ function getAgent() {
     }); 
 }
 
+function getMarketingPartner() {
+    settings = get_settings('marketing-partner/', 'GET');
+    $.ajax(settings).done(function (response) {
+        var data = JSON.parse(response);
+        
+        $('#lead_ppc_gold_buyer').val(data['lead_ppc_gold_buyer']);
+        $('#lead_ppl_gold_seller').val(data['lead_ppl_gold_seller']);
+        $('#lead_ppl_gold_buyer').val(data['lead_ppl_gold_buyer']);
+        $('#lead_ppc_gold_seller').val(data['lead_ppc_gold_seller']);
+
+        $('#lead_ppc_silver_buyer').val(data['lead_ppc_silver_buyer']);
+        $('#lead_ppl_silver_seller').val(data['lead_ppl_silver_seller']);
+        $('#lead_ppl_silver_buyer').val(data['lead_ppl_silver_buyer']);
+        $('#lead_ppc_silver_seller').val(data['lead_ppc_silver_seller']);
+
+        $('#lead_ppc_bronze_buyer').val(data['lead_ppc_bronze_buyer']);
+        $('#lead_ppl_bronze_seller').val(data['lead_ppl_bronze_seller']);
+        $('#lead_ppl_bronze_buyer').val(data['lead_ppl_bronze_buyer']);
+        $('#lead_ppc_bronze_seller').val(data['lead_ppc_bronze_seller']);
+
+        $('#referral_ppc_buyer').val(data['referral_ppc_buyer']);
+        $('#referral_ppc_seller').val(data['referral_ppc_seller']);
+    }); 
+}
+
 function initDescription(){
     tab_tutorial_json = JSON.parse(localStorage.getItem('tab_tutorial_json'));
     if (tab_tutorial_json['custom_link_des_hide']) {
@@ -49,11 +74,52 @@ $(document).ready(function(){
     getAgent();
     initDescription();
 
+    getMarketingPartner();
+
     $('.minimize-description').on('click', function(){
         displayDescription(false);
     });
 
     $('.maximize-description').on('click', function(){
         displayDescription();
+    });
+
+    $('#make-it-rain-btn').on('click', function(){
+        var data = {};
+        data['lead_ppc_gold_buyer'] = parseInt($('#lead_ppc_gold_buyer').val());
+        data['lead_ppl_gold_seller'] = parseInt($('#lead_ppl_gold_seller').val());
+        data['lead_ppl_gold_buyer'] = parseInt($('#lead_ppl_gold_buyer').val());
+        data['lead_ppc_gold_seller'] = parseInt($('#lead_ppc_gold_seller').val());
+
+        data['lead_ppc_silver_buyer'] = parseInt($('#lead_ppc_silver_buyer').val());
+        data['lead_ppl_silver_seller'] = parseInt($('#lead_ppl_silver_seller').val());
+        data['lead_ppl_silver_buyer'] = parseInt($('#lead_ppl_silver_buyer').val());
+        data['lead_ppc_silver_seller'] = parseInt($('#lead_ppc_silver_seller').val());
+
+        data['lead_ppc_bronze_buyer'] = parseInt($('#lead_ppc_bronze_buyer').val());
+        data['lead_ppl_bronze_seller'] = parseInt($('#lead_ppl_bronze_seller').val());
+        data['lead_ppl_bronze_buyer'] = parseInt($('#lead_ppl_bronze_buyer').val());
+        data['lead_ppc_bronze_seller'] = parseInt($('#lead_ppc_bronze_seller').val());
+
+        data['referral_ppc_buyer'] = parseInt($('#referral_ppc_buyer').val());
+        data['referral_ppc_seller'] = parseInt($('#referral_ppc_seller').val());
+
+        $('#partner-spinner').show();
+        $('#partner-check').hide();
+
+        settings = get_settings('marketing-partner/', 'POST', JSON.stringify(data));
+        $.ajax(settings).done(function (res) {
+            $('#partner-spinner').hide();
+            $('#partner-check').show();
+
+        }).fail(function (err) {
+            $('.msg').html(err['responseText']);
+            console.log(err);
+            alert(err);
+
+            $('#partner-spinner').hide();
+            $('#partner-check').hide();
+        });
+        
     });
 });
