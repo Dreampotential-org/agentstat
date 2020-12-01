da280b04b781038f42b7ec1c029f9ada29eb9f1a
Aaron Rosen <aaronorosen@gmail.com>
2020-11-30 22:12:56 -0800

    set_referral - wip

---
+++ www/js/ambassador.js
@@ -0,0 +1,18 @@
+function set_referral() {
+  var data = {
+    'by_agent': '25365'
+  }
+  settings = get_settings('set-referral/', 'POST', JSON.stringify(data))
+  $.ajax(settings).done(function (response) {
+    var msg = JSON.parse(response);
+    // console.log(msg);
+    console.log(msg['token']);
+    localStorage.session_id = msg['token'];
+    window.location = '/profile-settings/';
+  }).fail(function(err) {
+    alert(err);
+    show_error(err);
+    console.log(err);
+  });
+
+}
