6c1a12ca70699b09b7de6cbe2ef07b9f2bf08de9
Irfan Ullah <irfanullah@orangebuddies.pk>
2021-01-20 03:30:04 +0500

    c

--- www/js/agent-tabs.js
+++ www/js/agent-tabs.js
@@ -1,32 +1,31 @@
 function set_agent_tabs_default(data) {
-    if (data['scores'][0] != undefined) {
+    if (data['scores'][0] != undefined)  {
         $("#overall-avg-dom").html(data['overall_avg_dom'].toFixed(2));
         $("#overall-s2l-price").html(
-                '$' + data['overall_s2l_price'].toFixed(2) + '%');
-        var overSolidList = data['overall_sold_listings'];
-        $('.overall_sold_listings').html(overSolidList.toLocaleString());
+            '$'+data['overall_s2l_price'].toFixed(2) + '%');
+        $('.overall_sold_listings').html(data['overall_sold_listings']);
 
         $('.overall_score').html('-');
     }
-
+    
 }
 
 function ifFilterMatched() {
     if (Object.keys(matchedScoreObj).length > 0) {
         var successRate = calculateSuccessRate(matchedScoreObj['failed_listings'], matchedScoreObj['sold_listings']);
-        $('#overall-score').html(successRate + '%');
-
+        $('#overall-score').html(successRate+'%');
+        
 
         if (matchedScoreObj['avg_dom']) {
             var city_avg_dom = Math.round(matchedScoreObj['avg_dom']);
             $('#overall-avg-dom').html(city_avg_dom);
         }
-
+    
         if (matchedScoreObj['s2l_price']) {
             var s2l_price = Math.round(matchedScoreObj['s2l_price']);
             $('#overall-s2l-price').html(s2l_price + "%s");
         }
-
+       
         if (matchedScoreObj['sold_listings']) {
             $('.overall_sold_listings').html(matchedScoreObj['sold_listings']);
         }
