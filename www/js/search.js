99d7412e3ea9a14e638777446004ed2b4e6aea8d
Irfan Ullah <irfanullah@orangebuddies.pk>
2021-01-20 03:04:13 +0500

    number format fixing

--- www/js/search.js
+++ www/js/search.js
@@ -740,12 +740,15 @@ function getNationwideScore(time_duration = 36) {
     settings['headers'] = null;
     $.ajax(settings).done(function (response) {
         data = JSON.parse(response);
+        var soldList = data.total_sold_listing;
+        var singleFamily = data.single_family_sold_listing;
+        var condoFamily = data.condo_family_sold_listing;
         $('#nationwide-success-rate').html(data.success_rate_average.toFixed(2) + '%');
-        $('#nationwide-sold-listing').html(data.total_sold_listing);
+        $('#nationwide-sold-listing').html(soldList.toLocaleString());
         $('#nationwide-failed-listing').html(data.total_failed_listing);
         $('#nationwide-s2l-price').html(data.s2l_price_average.toFixed(2) + '%');
-        $('#nationwide-single-family-sold').html(data.single_family_sold_listing);
-        $('#nationwide-condo-family-sold').html(data.condo_family_sold_listing);
+        $('#nationwide-single-family-sold').html(singleFamily.toLocaleString());
+        $('#nationwide-condo-family-sold').html(condoFamily.toLocaleString());
     }).fail(function (err) {
         console.log(err);
     });
