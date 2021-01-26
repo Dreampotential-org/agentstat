13e123573d4104a66a3b80a3fdd03e725fdcb39f
Hassan Ejaz <hassanejaz22@gmail.com>
2021-01-26 17:27:59 +0500

    sitemap

--- www/js/sitemap.js
+++ www/js/sitemap.js
@@ -2,7 +2,8 @@ function populateStates() {
     $('.state-list').html('');
     var states = getStateList();
     $.each(states, function(k,v){
-        var link = '<li><a class="state-link" href="javascript:void(0)" data-state="'+k+'" >'+v+' • Homes for sale</a></li>';
+        var url = '/sitemap/'+k;
+        var link = '<li><a href="'+url+'" >'+v+' • Homes for sale</a></li>';
         $('.state-list').append(link);
     });
 }
@@ -11,7 +12,8 @@ function populateCities(state) {
     $('.city-list').html('');
     var cities = getCityListByState(state);
     $.each(cities, function(k,v){
-        var link = '<li><a class="city-link" href="javascript:void(0)" data-state="'+state+'" data-city="'+v+'" >'+v+' Real Estate</a></li>';
+        var url = '/sitemap/'+state+'/'+v;
+        var link = '<li><a href="'+url+'">'+v+' Real Estate</a></li>';
         $('.city-list').append(link);
     });
 }
@@ -27,51 +29,16 @@ function populateAgents(state, city) {
 }
 
 $(document).ready(function(){
-
-    populateStates();
-    populateCities('AK');
-    populateAgents('AK', 'Anchorage');
-
-    // city index function
-    $(".show-more").click(function () {
-        if($(".text").hasClass("show-more-height")) {
-            $(this).text("Show Less ˄");
-        } else {
-            $(this).text("Show More ˅");
-        }
-        $(".text").toggleClass("show-more-height");
-    });
-        
-    // state index function
-    $(".show-more1").click(function () {
-        if($(".text1").hasClass("show-more-height1")) {
-            $(this).text("Show Less ˄");
-        } else {
-            $(this).text("Show More ˅");
-        }
-        $(".text1").toggleClass("show-more-height1");
-    });
-
-    // ageny city index function
-    $(".show-more2").click(function () {
-        if($(".text2").hasClass("show-more-height2")) {
-            $(this).text("Show Less ˄");
-        } else {
-            $(this).text("Show More ˅");
-        }
-
-        $(".text2").toggleClass("show-more-height2");
-    });
-
-    $(document).on('click', '.state-link',function(){
-        var state = $(this).data('state');
-        populateCities(state);
-    });
-
-    $(document).on('click', '.city-link',function(){
-        var state = $(this).data('state');
-        var city = $(this).data('city');
-        populateAgents(state, city);
-    });
-
+    
+    var url = window.location.pathname.split("/");;
+    if (url[3]) {
+        $('#agent-cityindex').show();
+        populateAgents(url[2], url[3]);
+    } else if (url[2]) {
+        $('#cityindex').show();
+        populateCities(url[2]);
+    } else {
+        $('#state-index').show();
+        populateStates();
+    }
 });
