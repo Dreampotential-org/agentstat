33c55ec9aac2fb3fe6053d4db493dd4754a8eedf
root <root@node.arosen-161263.rethinkdb-pg0.clemson.cloudlab.us>
2023-07-29 23:25:53 -0600

    update

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
+export COMPOSE_HTTP_TIMEOUT=2000
 #!/bin/bash
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml build
--- www/js/conf.js
+++ www/js/conf.js
@@ -1,4 +1,5 @@
-SERVER_URL = 'https://app.agentstat.com/';
+//SERVER_URL = 'https://app.agentstat.com/';
+SERVER_URL = 'http://clnode068.clemson.cloudlab.us:8000/'
 
 // UNCOMMENT to connect to local django-zillow api instance
 SERVER_URL = 'http://localhost:8000/';
