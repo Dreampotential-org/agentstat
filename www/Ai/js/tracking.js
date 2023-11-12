5f1669f8478a91ab0d5da5d2bd6d012126e37dcb
Aaron Rosen <aaronorosen@gmail.com>
2023-11-12 18:03:32 -0500

    update ai url linking

--- www/Ai/js/tracking.js
+++
@@ -1,9 +0,0 @@
-function createAgentImpression(agent_id) {
-    settings = get_settings(
-	'agent_impression/'+ agent_id + '/', 'POST');
-    settings['headers'] = {};
-    settings['async'] = false;
-  
-    var jqXHR = $.ajax(settings);
-    console.log(JSON.parse(jqXHR.responseText))
-}
