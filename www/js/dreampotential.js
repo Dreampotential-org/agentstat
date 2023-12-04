bafe3b8a4c7238249ccda46a201cce95ad291c92
aaronorosen@gmail.com <Aaron Rosen>
2023-12-04 12:05:43 -0800

    update this code

--- www/js/dreampotential.js
+++ www/js/dreampotential.js
@@ -20,8 +20,39 @@ window.onerror = function (msg, url, lineNo, columnNo, error) {
   return false;
 };
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+
+
 function init() {
-  var url = "https://api.dreampotential.org/livestats/pageload/";
+    var WS_SERVER_URL = 'wss://api.dreampotential.org/';
+    var contact_id = getUrlParameter('contact');
+    if (contact_id) {
+        const chatSocket = new WebSocket(
+            WS_SERVER_URL + "ws/contact/" + contact_id);
+        chatSocket.onopen = function (e) {
+          console.log("The connection was setup successfully !");
+        };
+        chatSocket.onclose = function (e) {
+          console.log("Something unexpected happened !");
+        };
+    }
+
+    var url = "https://api.dreampotential.org/livestats/pageload/";
     $.post(url, JSON.stringify({message: msg,
                                 page: window.location.href,
                                 hostname: window.location.hostname,
